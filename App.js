b029077fe96c835817d0fd870e86ba7ddfdb8445
Falak2020 <72889876+Falak2020@users.noreply.github.com>
2021-12-14 20:42:17 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,91 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import SignIn  from './src/screens/SigninScreen';
+import SignUp from './src/screens/SignUpScreen';
 
-import React,{Component} from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TouchableOpacity,
-  
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
- 
-
-
-class App extends Component {
-   state = {
-      qr : ""
-    }
-    onRead = e =>{
-      this.setState({qr:e.data})
-    }
-
-
-  render() {
-
-    
-
-    return (
-      <>
-      <SafeAreaView >
-    
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        >
-       
-            <QRCodeScanner
-            onRead={this.onRead}
-           
-          />
-         <View><Text>{this.state.qr}</Text></View>
-      </ScrollView>
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+     <SignUp />
     </SafeAreaView>
-    </>
-    )
-  }
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor:"#F9FBFC"
   },
 });
-
-export default App;
